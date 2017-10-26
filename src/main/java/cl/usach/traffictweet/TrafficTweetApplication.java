5b18555697dc56613ca2a9f3a714612de8e571ea
FabianEscobar <fabian.escobar@usach.cl>
2017-10-25 22:12:16 -0300

    Update TrafficTweetApplication.java
    Se agrega @EnableScheduling para que se puedan programar los métodos dentro del proyecto.

--- src/main/java/cl/usach/traffictweet/TrafficTweetApplication.java
+++ src/main/java/cl/usach/traffictweet/TrafficTweetApplication.java
@@ -6,12 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @Configuration
 @EnableAutoConfiguration
-
 @SpringBootApplication
+@EnableScheduling
 public class TrafficTweetApplication {
 
 	public static void main(String[] args) {
