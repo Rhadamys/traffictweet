6e9153df63dc19f45a460810604b6cc2b87f0f14
Mario Álvarez Molina <mario.alvarez.m@usach.cl>
2017-10-28 18:28:28 -0300

    Folder names refactor

--- src/main/java/cl/usach/traffictweet/Repositories/KeywordRepository.java
+++
@@ -1,8 +0,0 @@
-package cl.usach.traffictweet.repositories;
-
-import cl.usach.traffictweet.models.Keyword;
-import org.springframework.data.repository.CrudRepository;
-
-public interface KeywordRepository extends CrudRepository<Keyword,Integer>{
-
-}
--- src/main/java/cl/usach/traffictweet/Repositories/StreetRepository.java
+++
@@ -1,8 +0,0 @@
-package cl.usach.traffictweet.repositories;
-
-import cl.usach.traffictweet.models.Street;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StreetRepository extends CrudRepository<Street,Integer>{
-    Street findByName(String name);
-}
